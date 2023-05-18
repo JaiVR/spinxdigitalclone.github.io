6714b9a36e3d4e879e61954b6fdce632a56c0e47
shaipranesh2 <f20200731@pilani.bits-pilani.ac.in>
2023-05-18 21:29:33 +0530

    deployed website

--- src/Nav.js
+++ src/Nav.js
@@ -1,34 +1,36 @@
 import React from "react";
 
 export default function Nav(){
-    function showDiv(id) {
-        document.getElementById(id).style.display = "block";
-      }
-      
-      function hideDiv(id) {
-        document.getElementById(id).style.display = "none";
-      }
+    
     return(
         <nav className="navbar">
+            <div className="nav-ele">
             <div className="logo">
               <a href="#"><img src="logo.svg" ></img></a>
             </div>
-            
-            <ul>
+            <div className="nav-links">
+             <ul>
                 <l><a href="#">WORK</a></l>
                 <l><a href="#">EXPERTISE</a></l>
-                <l onmouseover="showDiv(about-divi)" onmouseout="hideDiv(about-divi)"><a href="#">ABOUT</a></l>
-                <div id="about-divi"  className="about-div">
-                    <div className="about-1">
-                        <h2>Who we are</h2>
-                        <p>The secret ingredient for the SPINX difference.</p>
-                    </div>
-                </div>
+                <l>
+                    <a href="#">ABOUT</a>
+                    
+                    <ul className="drop-menu">
+                        <li><a href="#">Who We Are</a></li>
+                        <li><a href="#">Our Process</a></li>
+                        <li><a href="#">Careers</a></li>
+                    </ul>
+                    
+                </l>
                 <l><a href="#">INSIGHTS</a></l>
-            </ul>
-            <button className="start-project-button">
+             </ul>
+            </div>
+
+              <div  className="start-project-button"> <button>
                 <img className="button-image" src="inbox-logo.png"></img><h3 className="button-text">START A PROJECT</h3>
-            </button>
+                  </button>
+              </div>
+            </div>
         </nav>
     )
 }
